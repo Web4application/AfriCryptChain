a04cb0018730856b0d93b9d324cde5a9658a9b2d
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-11 21:42:34 -0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+--algorithm ethash --pool us-south01.miningrigrentals.com:3344 --wallet YOUR_WALLET --password x
+
+python africrypt_node.py
+
+mkdir build && cd build
+cmake ..
+make
+
+pip install ecdsa flask
+
+git clone https://github.com/Web4application/AfriCryptChain.git
+cd AfriCryptChain
